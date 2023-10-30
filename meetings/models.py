ea5dbec62047dc0f715eca7feb888535b26f04c1
prabhurams <prabhuram.s@gmail.com>
2023-10-30 14:08:21 +0530

    migrations works

--- meetings/models.py
+++ meetings/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import time
 
 # Create your models here.
+
+class Meetings(models.Model):
+    meeting_title = models.CharField(max_length=200)
+    meeting_date = models.DateField() 
+    
+    start_time = models.TimeField(default=time(9))
+    duration = models.IntegerField(default=1)
+
+    
+    def __str__(self):
+        return self.meeting_title
+    
