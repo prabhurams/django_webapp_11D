9b2f29f07ddc98b5754ce871c7178770056f057c
prabhurams <prabhuram.s@gmail.com>
2023-10-30 20:16:22 +0530

    added variables from database

--- website/views.py
+++ website/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from meetings.models import Meetings
+
 
 #def welcome(request):
    # return HttpResponse("Welcome to the Meeting Planner 3!")
 
-def welcome(request):
-    return render(request, "website/welcome.html")
+#def welcome(request):
+#    return render(request, "website/welcome.html", { "message1": " Does this data really come from view inside"})
  
+
+def welcome(request):
+    return render(request, "website/welcome.html",{"num_meetings": Meetings.objects.count() })
+
 def welcome2(request):
     return HttpResponse("Welcome to the Meeting Planner 3 in VS!")
 
 def about(request):
-    return HttpResponse("About Us Page")
+    return render(request,"website/about.html", {"message2": "about message from view_message2"})
+
+#def about(request):
+ #   return HttpResponse("About Us Page")
+
